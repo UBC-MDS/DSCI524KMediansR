5c6b8f03677c7fabd03faf6d315059d6ffa40de1
Ting Pan <tingpan-18dsba@outlook.com>
2019-02-08 14:56:27 -0800

    Add three functions with documentations

---
+++ R/kmedians_cluster.R
@@ -0,0 +1,24 @@
+kmedians <- function(X, num_clusters){
+  # Groups the points in your dataset ,X, into the desired number of clusters, based on the median distance between the points.
+  # This function uses random intilization to assign the first medians and then will update the medians and
+  # the group assignments until the assignment does not change.
+  #
+  # Parameters
+  # ----------
+  # X: 2D array
+  # The dataset being clustered
+  #
+  # num_clusters: integer
+  # The desired number of clusters
+  #
+  # Returns
+  # -------
+  # medians: 2D array
+  # The coordinates of the medians for each cluster
+  #
+  # labels: 1D array
+  # Array that has the assignment of the cluster for each point in the dataset
+
+
+  return (0)
+}

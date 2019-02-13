eb38f47305c49a8f9323916ee6ee4cd1d70116b3
Ting Pan <tingpan-18dsba@outlook.com>
2019-02-13 13:56:16 -0800

    add summary function

--- R/kmedians_summary.R
+++ R/kmedians_summary.R
@@ -22,6 +22,28 @@ summary <- function(X, medians, labels){
   # Returns a dataframe with 6 columns and number of rows will be the number of clusters. The labels of the columns:
   # Cluster labels, Median Coordinates, Number of Points in Cluster, Average Distance, Minimum Distance, Maximum Distance
 
+  medians_df <- data.frame(cbind(unique(labels),medians)) %>%
+    plyr::rename(c(X1 = 'label', X2 = 'medianX', X3 = 'medianY'))
 
-  return (0)
+  summary_df <- data.frame(cbind(A,labels)) %>%
+    plyr::rename(c(V1 = 'X', V2 = 'Y', labels = 'label')) %>%
+    dplyr::right_join(medians_df, by = 'label') %>%
+    dplyr::mutate(distance = (abs(X-medianX)+abs(Y-medianY))) %>%
+    dplyr::select(label, medianX, medianY, distance) %>%
+    dplyr::group_by(label) %>%
+    dplyr::summarise(medianX = unique(medianX), medianY = unique(medianY), num = n(), avgd = mean(distance), mind = min(distance), maxd = max(distance)) %>%
+    dplyr::mutate(med = paste(medianX, medianY, sep = ",")) %>%
+    dplyr::arrange(label) %>%
+    dplyr::select(label, med, num, avgd, mind, maxd) %>%
+    plyr::rename(c(label = 'Cluster Label',
+                   med = 'Median Coordinates',
+                   num = 'Number of Points in Cluster',
+                   avgd = 'Average Distance',
+                   mind = 'Minimum Distance',
+                   maxd = 'Maximum Distance'))
+
+  return (data.frame(summary_df))
+
 }
+
+

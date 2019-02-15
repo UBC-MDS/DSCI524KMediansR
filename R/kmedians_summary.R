7eef50e658d34d78f61ac1f15e8bc4a5a1cd8153
fan0521 <fan_nie@hotmail.com>
2019-02-15 02:50:18 -0800

    added r functions

--- R/kmedians_summary.R
+++ R/kmedians_summary.R
@@ -1,28 +1,20 @@
-#' @import tidyverse
-
+#' summary
+#'
+#' Generates a table to display the cluster labels, the coordinates of the cluster medians,
+#' number of points in each cluster, the average distance within the cluster,
+#' the maximum distance within the cluster and the minimum distance within the cluster.
+#'
+#' @param X matrix,the dataset being clustered
+#' @param medians matrix, coordinates of each cluster median
+#' @param labels list, array with the assignment of the cluster for each point in the dataset
+#'
+#' @return dataframe. Returns a dataframe with 6 columns and number of rows will be the number of clusters. The labels of the columns:
+#'          Cluster labels, Median Coordinates, Number of Points in Cluster, Average Distance, Minimum Distance, Maximum Distance
+#' @export
+#'
+#' @examples
+#'
 summary <- function(X, medians, labels){
-  # Generates a table to display the cluster labels, the coordinates of the cluster medians,
-  # number of points in each cluster, the average distance within the cluster,
-  # the maximum distance within the cluster and the minimum distance within the cluster.
-  #
-  #
-  # Parameters
-  # ----------
-  #
-  # X: matrix
-  # The dataset being clustered
-  #
-  # medians: matrix
-  # Coordinates of each cluster median
-  #
-  # labels:  list
-  # Array with the assignment of the cluster for each point in the dataset
-  #
-  # Returns
-  # -------
-  # dataframe
-  # Returns a dataframe with 6 columns and number of rows will be the number of clusters. The labels of the columns:
-  # Cluster labels, Median Coordinates, Number of Points in Cluster, Average Distance, Minimum Distance, Maximum Distance
 
   medians_df <- data.frame(cbind(unique(labels),medians))
   colnames(medians_df) <- c("label", "medianX", "medianY")
@@ -44,4 +36,6 @@ summary <- function(X, medians, labels){
                             "Number of Points in Cluster","Average Distance",
                             "Minimum Distance","Maximum Distance")
 
-  return
+  return (data.frame(summary_df))
+
+}
